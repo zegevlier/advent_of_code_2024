be6ea534c65d4045bd6c14510adf8131fe323542
zegevlier <iamascratcher@gmail.com>
2024-12-01 17:38:05 +0100

    Improve speed of part2

--- src/day1.rs
+++ src/day1.rs
@@ -28,17 +28,40 @@ pub fn part1(input: &str) -> i32 {
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &str) -> i32 {
-    let mut l1: Vec<i32> = vec![];
-    let mut l2: Vec<i32> = vec![];
+pub fn part2(input: &str) -> usize {
+    let lines = input.lines();
+    // We can't get the size without consuming the iterator, which would be very slow.
+    // So, we just initialise it at 1k and hope that's roughly correct.
+    let mut l1: Vec<usize> = Vec::with_capacity(1000);
+    let mut l2: Vec<usize> = Vec::with_capacity(1000);
 
-    input.lines().for_each(|line| {
-        let mut line_split = line.split("   ");
-        l1.push(line_split.next().unwrap().parse().unwrap());
-        l2.push(line_split.next().unwrap().parse().unwrap());
-    });
+    for line in lines {
+        // Splitting on a char is *much* faster than splitting on a string. We know the separator is 3 spaces, so we just ignore the other two.
+        // If we know for sure the input is always 5 chars longs, we can make this parsing even faster, but we don't.
+        let line_split = line.split_once(' ').unwrap();
+        l1.push(line_split.0.parse().unwrap());
+        l2.push(line_split.1[2..].parse().unwrap());
+    }
 
-    l1.iter()
-        .map(|v| v * l2.iter().filter(|r| *r == v).count() as i32)
-        .sum()
+    // There might be a faster way of sorting these, but this is decently fast.
+    l1.sort();
+    l2.sort();
+
+    let mut total = 0;
+    let mut previous_value = 0;
+    let mut previous_s = 0;
+    for value in l1 {
+        if value == previous_value {
+            total += previous_s;
+            continue;
+        }
+        previous_value = value;
+        // We find the value in the other list using binary search
+        let low = l2.partition_point(|x| x < &value);
+        let high = l2.partition_point(|x| x <= &value);
+        previous_s = value * (high - low);
+        total += previous_s;
+    }
+
+    total
 }
