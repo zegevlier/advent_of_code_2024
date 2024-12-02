0bff4d9e795f5322a15cc516690129c400c9ed93
zegevlier <iamascratcher@gmail.com>
2024-12-02 23:10:58 +0100

    Fix issues with d2p2

--- src/day2.rs
+++ src/day2.rs
@@ -28,7 +28,7 @@ pub fn part1(input: &str) -> i32 {
                 }
                 // Now we check if it's in range
                 let diff = ((pv - value) as i32).abs();
-                if diff > 3 || diff < 1 {
+                if !(1..=3).contains(&diff) {
                     break;
                 }
             };
@@ -39,95 +39,78 @@ pub fn part1(input: &str) -> i32 {
     total
 }
 
+fn verify_line(line: Vec<&str>, grace_given: bool) -> bool {
+    let mut prev_value: Option<i32> = None;
+    let mut goes_up = None;
+
+    let mut line_iter = line.iter();
+    let mut i = 0;
+    loop {
+        let value: i32 = match line_iter.next() {
+            Some(v) => v.parse().unwrap(),
+            None => {
+                // We're at the end of the list. This one is valid.
+                return true;
+            }
+        };
+        if let Some(pv) = prev_value {
+            // We have a previous value, so we need to ensure it's in range. Let's first ensure it goes up and down as expected.
+            if let Some(gu) = goes_up {
+                if gu == (pv > value) {
+                    if !grace_given {
+                        // We need to re-check this list with the value we got stuck on removed.
+                        // Note, we may also need to remove the value just before the one where we got stuck.
+
+                        // This is painful, but I don't have time to fix it today. Might try later!
+                        let new_line = [line[0..i].to_vec(), line[i + 1..].to_vec()].concat();
+                        if verify_line(new_line, true) {
+                            return true;
+                        }
+                        let new_line = [line[0..i - 1].to_vec(), line[i..].to_vec()].concat();
+                        if verify_line(new_line, true) {
+                            return true;
+                        }
+                        let new_line = [line[0..i - 2].to_vec(), line[i-1..].to_vec()].concat();
+                        return verify_line(new_line, true);
+                    }
+                    return false;
+                }
+            } else {
+                goes_up = Some(pv < value)
+            }
+            // Now we check if it's in range
+            let diff = (pv - value).abs();
+            if !(1..=3).contains(&diff) {
+                if !grace_given {
+                    // We need to re-check this list with the value we got stuck on removed.
+                    // Note, we may also need to remove the value just before the one where we got stuck.
+                    let new_line = [line[0..i].to_vec(), line[i + 1..].to_vec()].concat();
+                    if verify_line(new_line, true) {
+                        return true;
+                    }
+                    let new_line = [line[0..i - 1].to_vec(), line[i..].to_vec()].concat();
+                    return verify_line(new_line, true);
+                }
+                return false;
+            }
+        };
+        prev_value = Some(value);
+        i += 1;
+    }
+}
+
 #[aoc(day2, part2)]
 pub fn part2(input: &str) -> i32 {
     let mut total = 0;
 
     for line in input.lines() {
-        let mut line_split = line.split(' ').peekable();
-        let mut prev_value = None;
-        let mut goes_up: Option<bool> = None;
-        let mut grace_given = false;
-        let mut first_value = None;
-        let mut i = 0;
-        loop {
-            let value: i32 = match line_split.next() {
-                Some(v) => v.parse().unwrap(),
-                None => {
-                    total += 1;
-                    break;
-                }
-            };
-            if let Some(mut pv) = prev_value {
-                // We have a previous value, so we need to ensure it's in range. Let's first ensure it goes up and down as expected.
-                match goes_up {
-                    Some(gu) => {
-                        if gu == (pv > value) {
-                            if grace_given {
-                                break;
-                            } else {
-                                grace_given = true;
-                                // If this is not the third element in the list, we just remove it and see if it works now.
-                                if i != 2 {
-                                    continue;
-                                }
-                                // If this is the third element in the list, we could either remove the second or the third one.
-                                // We can determine whether we need to do this by looking at the next available value,
-                                // if it goes the same direction as the second, we remove the third. If it goes the same direction
-                                // as the third, we remove the second.
-                                let next_value = line_split.peek().unwrap().parse::<i32>().unwrap();
-                                // first_value is for 0
-                                // pv is for 1
-                                // value is for 2
-                                // next_value is for 3
-                                // gu is for 0 compared to 1
-                                // gu != first_value < value
-                                if gu == (first_value.unwrap() < next_value) {
-                                    // We need to remove 2, since the next one lines up with the one after.
-                                    // Just continuing here does this.
-                                    continue;
-                                } else {
-                                    // We need to remove 1. This means resetting the pv to the first value, and updating the goes up.
-                                    goes_up = Some(!gu);
-                                    prev_value = first_value;
-                                    pv = first_value.unwrap();
-                                    // We need to do the rest of the checks here.
-                                }
-                            }
-                        }
-                    }
-                    None => goes_up = Some(pv < value),
-                }
-                // Now we check if it's in range
-                let diff = ((pv - value) as i32).abs();
-                if diff > 3 || diff < 1 {
-                    if grace_given {
-                        break;
-                    } else {
-                        grace_given = true;
-                        if i != 1 {
-                            continue;
-                        }
-                        goes_up = None;
-                        // We might need to remove the first one instead of the second one. 
-                        let next_value = line_split.peek().unwrap().parse::<i32>().unwrap();
-                        // If the next value is in range compared to the current value, we remove the first value.
-                        // No need to update first_value, because we've already given grace here.
-                        let diff = ((value - next_value) as i32).abs();
-                        if diff > 3 || diff < 1 {
-                            // We remove the second value instead.
-                            continue;
-                        } else {
-                            // We need to remove the first value
-                            // We do this by allowing it to set the prev value.
-                        }
-                    }
-                }
-            } else {
-                first_value = Some(value);
-            };
-            prev_value = Some(value);
-            i += 1;
+        let line_split = line.split(' ').collect();
+
+        if verify_line(line_split, false) {
+            dbg!("GOOD", line);
+            total += 1;
+        } else {
+            dbg!("BAD", line);
         }
     }
 
@@ -148,6 +131,18 @@ mod test {
 1 10 11 12 13
 9 1 2 3 4";
 
+    // 💖 https://www.reddit.com/r/adventofcode/comments/1h4shdu/2024_day_2_part2_edge_case_finder/
+    const EDGECASE_FROM_REDDIT: &str = "48 46 47 49 51 54 56
+1 1 2 3 4 5
+1 2 3 4 5 5
+5 1 2 3 4 5
+1 4 3 2 1
+1 6 7 8 9
+1 2 3 4 3
+9 8 7 6 7
+7 10 8 10 11
+29 28 27 25 26 25 22 20";
+
     #[test]
     fn test_part1() {
         assert_eq!(part1(INPUT), 2);
@@ -156,5 +151,10 @@ mod test {
     #[test]
     fn test_part2() {
         assert_eq!(part2(INPUT), 7);
+    }
+
+    #[test]
+    fn test_part2_edgecases() {
+        assert_eq!(part2(EDGECASE_FROM_REDDIT), 10);
     }
 }
